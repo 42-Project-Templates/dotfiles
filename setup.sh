f287d22bae64af86391b2968ac96e066cc1f094b
Abdulselam Karahan <akarahan096@gmail.com>
2024-01-20 17:49:09 +0000

    add basic setup: tmux and vim config with setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function install_omz_plugins() {
+	if [[ ! -d $HOME/zsh-syntax-highlighting ]]; then
+		echo "Installing zsh-syntax-highlighting"
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+		echo "Installed zsh-syntax-highlighting"
+	fi
+}
+
+if [[ ! -f $HOME/.vimrc ]]; then
+	ln -sfn $HOME/.dotfiles/.vimrc $HOME/.vimrc
+fi
+
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
